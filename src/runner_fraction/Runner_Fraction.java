77105e91d28a41c549aa018bc4819add4c20de19
rkaune <rkaune@ugcloud.ca>
2022-02-24 13:18:47 -0500

    Update Runner_Fraction.java
    Updated header

--- src/runner_fraction/Runner_Fraction.java
+++ src/runner_fraction/Runner_Fraction.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Mr. Kaune
+ * February 24, 2022
+ * Fraction from Module 5
  */
 package runner_fraction;
 
